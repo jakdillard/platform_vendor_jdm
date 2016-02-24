8f8c65a91291eb7f815948f9be665b21a09b09f8
jakdillard <jakdillard@gmail.com>
2016-02-24 14:56:54 -0500

    add google device build props

--- config/common.mk
+++ config/common.mk
@@ -2,3 +2,12 @@
 
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/jakdillard/overlay/common
+
+# Google Device specific
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1
