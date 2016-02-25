b1ae47ff97e0bde889948dcc1bef57de25a9f3b6
jakdillard <jakdillard@gmail.com>
2016-02-24 19:03:46 -0500

    add space for additional packages

--- config/common.mk
+++ config/common.mk
@@ -3,6 +3,9 @@
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/jdm/overlay/common
 
+# Additional Packages
+PRODUCT_PACKAGES += \
+
 # Google Device specific
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
