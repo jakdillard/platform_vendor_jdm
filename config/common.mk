f2fc5cce1cced004592c651c3cf21ddd05035d18
jakdillard <jakdillard@gmail.com>
2016-02-28 02:12:43 -0500

    add init.d and sysinit support

--- config/common.mk
+++ config/common.mk
@@ -23,3 +23,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/jdm/prebuilt/supersu/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
     vendor/jdm/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
+# init.d script support
+PRODUCT_COPY_FILES += \
+    vendor/jdm/prebuilt/bin/sysinit:system/bin/sysinit
+
+# Clean JDM init file
+PRODUCT_COPY_FILES += \
+    vendor/jdm/prebuilt/etc/init.jdm.rc:root/init.jdm.rc
