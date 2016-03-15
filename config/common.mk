993f6f583de531ec0ab3292f3b114d0c75d4813a
jakdillard <jakdillard@gmail.com>
2016-03-15 19:54:19 -0400

    add latinime overlay and lib file for gesture typing

--- config/common.mk
+++ config/common.mk
@@ -27,3 +27,12 @@ PRODUCT_COPY_FILES += \
     vendor/jdm/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/jdm/prebuilt/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/jdm/prebuilt/bin/50-base.sh:system/addon.d/50-base.sh
+
+# Bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/jdm/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+
+# Latin IME gesture typing 
+PRODUCT_COPY_FILES += \
+    vendor/jdm/prebuilt/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinime.so
+
