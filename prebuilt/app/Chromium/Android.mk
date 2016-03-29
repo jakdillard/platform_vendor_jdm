5f659f0ca6e133088989d986a69fd24be9c59127
jakdillard <jakdillard@gmail.com>
2016-03-28 20:21:54 -0400

    add prebuilt chromium then build it

---
+++ prebuilt/app/Chromium/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Chromium
+LOCAL_SRC_FILES := ChromePublic.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MULTILIB := both
+include $(BUILD_PREBUILT)
